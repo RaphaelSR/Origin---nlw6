46f43f0faafb3725216097edffa422d27755f2b9
Raphael Souza Rocha <raphaelrochabcc@gmail.com>
2021-07-15 19:21:54 -0300

    part 4 ok, mobile layout finished

--- main.js
+++ main.js
@@ -19,43 +19,64 @@ for (const link of links) {
 }
 
 /* Mudar o header da página quando der scroll */
-const header = document.querySelector("#header")
-const navHeight = header.offsetHeight
+function changeHeaderWhenScroll() {
+  const header = document.querySelector("#header");
+  const navHeight = header.offsetHeight;
+  if (window.scrollY >= navHeight) {
+    header.classList.add("scroll");
+    // scrool é maior que a altura do header
+  } else {
+    header.classList.remove("scroll");
+    // scrool é menor que a altura do header
+  }
+}
 
-window.addEventListener('scroll', function() {
-    if(window.scrollY >= navHeight){
-        header.classList.add("scroll")
-        // scrool é maior que a altura do header
-    }else{
-        header.classList.remove("scroll")
-        // scrool é menor que a altura do header
-    }
-})
+
+/* Botão voltar para o topo */
+function backToTop(){
+  const backToTopButton = document.querySelector(".back-to-top");
+  if (window.scrollY >= 560) {
+    backToTopButton.classList.add("show");
+  } else {
+    backToTopButton.classList.remove("show");
+  }
+}
+
+
 
 /* Testimonials carousel swiper slider */
-
-const swiper = new Swiper('.swiper-container', {
+const swiper = new Swiper(".swiper-container", {
   slidesPerView: 1,
   pagination: {
-    el: '.swiper-pagination'
+    el: ".swiper-pagination",
   },
   mousewheel: true,
   Keyboard: true,
 });
 
-
 /* ScrollReveal: Mostrar elementos quando der scroll na página */
 const scrollReveal = ScrollReveal({
-origin: 'top',
-distance: '30px',
-duration: 700,
-reset: true
-})
+  origin: "top",
+  distance: "30px",
+  duration: 700,
+  reset: true,
+});
 
 scrollReveal.reveal(
   `#home .text, #home .image,
   #about .image, #about .text,
   #services header, #services .card,
   #testimonials header, #testimonials .testimonials,
-  #contact .text, #contact .links
-  `, {interval: 100})
+  #contact .text, #contact .links,
+  footer .brand, footer .social
+  `,
+  { interval: 100 }
+);
+
+
+
+/* When Scroll */
+window.addEventListener("scroll", function(){
+  changeHeaderWhenScroll()
+  backToTop()
+});
