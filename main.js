15efcb6c5d98042f3c02e53fb8ec599da2677627
Raphael Souza Rocha <raphaelrochabcc@gmail.com>
2021-07-20 23:28:05 -0300

    desktop model ok - project finished

--- main.js
+++ main.js
@@ -19,9 +19,9 @@ for (const link of links) {
 }
 
 /* Mudar o header da página quando der scroll */
+const header = document.querySelector("#header");
+const navHeight = header.offsetHeight;
 function changeHeaderWhenScroll() {
-  const header = document.querySelector("#header");
-  const navHeight = header.offsetHeight;
   if (window.scrollY >= navHeight) {
     header.classList.add("scroll");
     // scrool é maior que a altura do header
@@ -31,18 +31,15 @@ function changeHeaderWhenScroll() {
   }
 }
 
-
 /* Botão voltar para o topo */
-function backToTop(){
-  const backToTopButton = document.querySelector(".back-to-top");
+const backToTopButton = document.querySelector(".back-to-top");
+function backToTop() {
   if (window.scrollY >= 560) {
     backToTopButton.classList.add("show");
   } else {
     backToTopButton.classList.remove("show");
   }
 }
-
-
 
 /* Testimonials carousel swiper slider */
 const swiper = new Swiper(".swiper-container", {
@@ -52,6 +49,12 @@ const swiper = new Swiper(".swiper-container", {
   },
   mousewheel: true,
   Keyboard: true,
+  breakpoints: {
+    767: {
+      slidesPerView: 2,
+      setWrapperSize: true,
+    },
+  },
 });
 
 /* ScrollReveal: Mostrar elementos quando der scroll na página */
@@ -73,10 +76,34 @@ scrollReveal.reveal(
   { interval: 100 }
 );
 
+/* Menu ativo conforme a seção visível na página */
+const sections = document.querySelectorAll('main section[id]')
+function activateMenuAtCurrentSection() {
+  const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4
 
+  for (const section of sections) {
+    const sectionTop = section.offsetTop
+    const sectionHeight = section.offsetHeight
+    const sectionId = section.getAttribute('id')
+
+    const checkpointStart = checkpoint >= sectionTop
+    const checkpointEnd = checkpoint <= sectionTop + sectionHeight
+
+    if (checkpointStart && checkpointEnd) {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.add('active')
+    } else {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.remove('active')
+    }
+  }
+}
 
 /* When Scroll */
-window.addEventListener("scroll", function(){
-  changeHeaderWhenScroll()
-  backToTop()
+window.addEventListener("scroll", function () {
+  changeHeaderWhenScroll();
+  backToTop();
+  activateMenuAtCurrentSection();
 });
